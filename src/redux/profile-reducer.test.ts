7082752b1210fc0f6e91be12d2d8b6d39f539d16
valeria-ab <lera.magnum@gmail.com>
2021-11-19 16:06:22 +0300

    added tests for profile-reducer

---
+++ src/redux/profile-reducer.test.ts
@@ -0,0 +1,46 @@
+import profileReducer, {
+  AddPostActionCreator,
+  DeletePostActionCreator,
+} from "./profile-reducer";
+
+it("new post should be added", () => {
+  // 1. test data
+  const action = AddPostActionCreator("New post");
+  const startState = {
+    profile: null,
+    postsData: [
+      { id: "1", postMessage: "Hi! It's my first post", likesCount: 3 },
+      { id: "2", postMessage: "Yo!", likesCount: 12 },
+    ],
+    isAuth: false,
+    status: "",
+  };
+
+  // 2. action data
+  const newState = profileReducer(startState, action);
+
+  // 3. expectation
+  expect(newState.postsData.length).toBe(3);
+  expect(newState.postsData[2].postMessage).toBe("New post");
+});
+
+it("after deleting length of messages should be decremented", () => {
+  // 1. test data
+  const action = DeletePostActionCreator("3");
+  const startState = {
+    profile: null,
+    postsData: [
+      { id: "1", postMessage: "Hi! It's my first post", likesCount: 3 },
+      { id: "2", postMessage: "Yo!", likesCount: 12 },
+    ],
+    isAuth: false,
+    status: "",
+  };
+
+  // 2. action data
+  const newState = profileReducer(startState, action);
+
+  // 3. expectation
+  expect(newState.postsData.length).toBe(2);
+  
+});
